ce23239dee43b19148217277216904cda0832506
Brent Reno <brent.reno@gmail.com>
2016-09-19 16:07:11 -0500

    update create_database with initial inserts

--- create_database.sql
+++ create_database.sql
@@ -7,4 +7,8 @@ CREATE TABLE treat(
   description text,
   pic text
 )
-INSERT INTO treats(name, description, pic) VALUES('Cupcake', 'Yum, a cupcake!', '/assets/cupcake.jpg');
+INSERT INTO treat(name, description, pic) VALUES('Cupcake', 'Yum, a cupcake!', '/assets/cupcake.jpg');
+INSERT INTO treat(name, description, pic) VALUES('Donuts', 'MMMMMM, Donuts... ', '/assets/donuts.jpg');
+INSERT INTO treat(name, description, pic) VALUES('Goldfish', '"The snack that smiles back" ', '/assets/goldfish.jpg');
+INSERT INTO treat(name, description, pic) VALUES('Ice Cream', 'We all scream for ice cream!', '/assets/icecream.jpg');
+INSERT INTO treat(name, description, pic) VALUES('Potato Chips', 'Bet you cant have just one.', '/assets/potato.jpg');
